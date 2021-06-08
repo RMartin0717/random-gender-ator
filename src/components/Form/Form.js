cb99669412422f86ec57066a0a36284fe6ed1756
Riley Martin <riley.willow@gmail.com>
2021-06-08 16:53:37 -0600

    Fix drop down options

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -17,14 +17,16 @@ class Form extends Component {
           <label for='vibe-select'>Choose a vibe:</label>
           <select name='vibes' id='vibe-select'>
             <option value=''>--Please select a vibe--</option>
-            <option value='trash'>Trash</option>
-            <option value='sparkle'>Sparkle</option>
+            <option value='trash'>Sparkle</option>
+            <option value='sparkle'>Trash</option>
+            <option value='sparkle-trash'>A Bit of Both</option>
           </select>
           <label for='entity-select'>Choose an entity:</label>
           <select name='vibes' id='entity-select'>
             <option value=''>--Please select an entity--</option>
-            <option value='trash'>Animal</option>
-            <option value='sparkle'>Extraterrestrial</option>
+            <option value='animal'>Animal</option>
+            <option value='extraterrestrial'>Extraterrestrial</option>
+            <option value='animal-extraterrestrial'>A Bit of Both</option>
           </select>
           <button>New Gender Please</button>
         </form>
