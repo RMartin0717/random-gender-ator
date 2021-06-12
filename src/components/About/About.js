c470e2b530ad509f1a66089f76bbd45656f10626
Riley Martin <riley.willow@gmail.com>
2021-06-12 16:54:32 -0600

    Create general layout for about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,19 +2,34 @@ import './About.css'
 
 const About = () => {
   return (
-    <>
+    <div className='about-page'>
       <h3>Want some ideas for how you might use this app?</h3>
-      <p>How do you reconcile the fact that gender as a construct is fake with the fact that your gender is valid? Play and find out.</p>
-
-      <p>There are several ways to approach The Random Gender-ator.</p>
-
-      <p>It can be a fun, silly way to test the boundaries of what gender can be to you as an individual even if you've never thought about it before. What "feels" like you? Does "froth squirrel" resonate with you in some special way?</p>
-
-      <p>Or think about the Gender-ator as a criticism of the societal pressure on the individual to fit into a box. What even are labels? Whatever you want, that's what.</p>
-      <p>Maybe for you, the Gender-ator is an escape from the gatekeeping that exists even in queer spaces. Why do none of us ever feel "queer enough"? Now I'm a "light-disposable spaceship." Try to stop me.</p>
-      <p>It also works as a light-hearted response to the heavy-handed, "there are SO MANY new terms, I just CAN'T EVEN keep up" that so many of us hear from loved ones who cant be bothered to try to see us as we see ourselves. Too many gender words? FINE. Have even more gender words.</p>
-      <p>Wherever you're coming from, play around a save some favorites. Use this app to goof off, find yourself, or tell someone to fuck off. Whatever you need.</p>
-    </>
+      <p className='focus-q about-p'>How do you reconcile the fact that gender as a construct is fake with the fact that your gender is valid? Play and find out.</p>
+      <p className='about-p'>There are several ways to approach The Random Gender-ator.</p>
+      <div className='ways-to-play'>
+        <div className='about-p'>
+          <p>It can be a fun, silly way to test the boundaries of what gender can be to you as an individual even if you've never thought about it before.</p>
+          <p>What "feels" like you?</p>
+          <p>Does "froth squirrel" resonate with you in some special way?</p>
+        </div>
+        <div className='about-p'>
+          <p>Or think about the Gender-ator as a criticism of the societal pressure on the individual to fit into a box.</p>
+          <p>What even are labels? Whatever you want, that's what.</p>
+        </div>
+        <div className='about-p'>
+          <p>Maybe for you, the Gender-ator is an escape from the gatekeeping that exists even in queer spaces.</p>
+          <p>Why do none of us ever feel "queer enough"? Now I'm a "light-disposable spaceship." Try to stop me.</p>
+          <p>Try to stop me.</p>
+        </div>
+        <div className='about-p'>
+          <p>It also works as a light-hearted response to the heavy-handed, "there are SO MANY new terms, I just CAN'T EVEN keep up" that so many of us hear from loved ones who cant be bothered to try to see us as we see ourselves.</p>
+          <p>Too many gender words?</p>
+          <p>FINE.</p>
+          <p>Have even more gender words.</p>
+        </div>
+      </div>
+      <p className='about-p'>Wherever you're coming from, play around a save some favorites. Use this app to goof off, find yourself, or tell someone to fuck off. Whatever you need.</p>
+    </div>
   )
 }
 
