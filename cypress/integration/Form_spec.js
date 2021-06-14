fd0dab24770aed1919881d54591ab29904894602
Riley Martin <riley.willow@gmail.com>
2021-06-14 09:40:05 -0600

    Implement intercepts for animal and trash fetch calls

--- cypress/integration/Form_spec.js
+++ cypress/integration/Form_spec.js
@@ -1,18 +1,20 @@
 describe('Form', () => {
   beforeEach(() => {
     cy.fixture('animal').then((animal) => {
-      cy.intercept('https://twinword-word-associations-v1.p.rapidapi.com/associations/?entry=animal',
-      headers: {
-    		x-rapidapi-key: "2f02989ae5msh427de8f8ba15f4ep120743jsn069d9b1838e3",
-    		x-rapidapi-host: "twinword-word-associations-v1.p.rapidapi.com"
-    	}).as('animal')
+      cy.intercept('https://twinword-word-associations-v1.p.rapidapi.com/associations/?entry=animal', {
+        body: animal,
+        headers: {
+      		'x-rapidapi-key': '2f02989ae5msh427de8f8ba15f4ep120743jsn069d9b1838e3',
+      		'x-rapidapi-host': 'twinword-word-associations-v1.p.rapidapi.com'
+      	}}).as('animal')
     })
     cy.fixture('trash').then((trash) => {
-      cy.intercept('https://twinword-word-associations-v1.p.rapidapi.com/associations/?entry=trash',
-      headers: {
-    		x-rapidapi-key: "2f02989ae5msh427de8f8ba15f4ep120743jsn069d9b1838e3",
-    		x-rapidapi-host: "twinword-word-associations-v1.p.rapidapi.com"
-    	}).as('trash')
+      cy.intercept('https://twinword-word-associations-v1.p.rapidapi.com/associations/?entry=trash', {
+        body: trash,
+        headers: {
+      		'x-rapidapi-key': '2f02989ae5msh427de8f8ba15f4ep120743jsn069d9b1838e3',
+      		'x-rapidapi-host': 'twinword-word-associations-v1.p.rapidapi.com'
+      	}}).as('trash')
     })
     //intercept for all 4 fetch calls
     //include header info in intercept
