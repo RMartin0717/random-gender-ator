a08c18ffca7330f03251156fc2f5e5e1eadec5c9
Riley Martin <riley.willow@gmail.com>
2022-07-30 15:26:31 -0500

    Update test file intercept routes

--- cypress/integration/About_spec.js
+++ cypress/integration/About_spec.js
@@ -1,36 +1,24 @@
 describe('About', () => {
   beforeEach(() => {
     cy.fixture('animal').then((animal) => {
-      cy.intercept('https://twinword-word-associations-v1.p.rapidapi.com/associations/?entry=animal', {
-        body: animal,
-        headers: {
-      		'x-rapidapi-key': '2f02989ae5msh427de8f8ba15f4ep120743jsn069d9b1838e3',
-      		'x-rapidapi-host': 'twinword-word-associations-v1.p.rapidapi.com'
-      	}}).as('animal')
+      cy.intercept('https://the-random-gender-ator-api.herokuapp.com/api/v1/animals', {
+        body: animal
+      }).as('animal')
     })
       .fixture('extraterrestrial').then((extraterrestrial) => {
-      cy.intercept('https://twinword-word-associations-v1.p.rapidapi.com/associations/?entry=extraterrestrial', {
-        body: extraterrestrial,
-        headers: {
-      		'x-rapidapi-key': '2f02989ae5msh427de8f8ba15f4ep120743jsn069d9b1838e3',
-      		'x-rapidapi-host': 'twinword-word-associations-v1.p.rapidapi.com'
-      	}}).as('extraterrestrial')
+      cy.intercept('https://the-random-gender-ator-api.herokuapp.com/api/v1/extraterrestrials', {
+        body: extraterrestrial
+      }).as('extraterrestrial')
     })
       .fixture('trash').then((trash) => {
-      cy.intercept('https://twinword-word-associations-v1.p.rapidapi.com/associations/?entry=trash', {
-        body: trash,
-        headers: {
-      		'x-rapidapi-key': '2f02989ae5msh427de8f8ba15f4ep120743jsn069d9b1838e3',
-      		'x-rapidapi-host': 'twinword-word-associations-v1.p.rapidapi.com'
-      	}}).as('trash')
+      cy.intercept('https://the-random-gender-ator-api.herokuapp.com/api/v1/trashes', {
+        body: trash
+      }).as('trash')
     })
       .fixture('sparkle').then((sparkle) => {
-      cy.intercept('https://twinword-word-associations-v1.p.rapidapi.com/associations/?entry=sparkle', {
-        body: sparkle,
-        headers: {
-      		'x-rapidapi-key': '2f02989ae5msh427de8f8ba15f4ep120743jsn069d9b1838e3',
-      		'x-rapidapi-host': 'twinword-word-associations-v1.p.rapidapi.com'
-      	}}).as('sparkle')
+      cy.intercept('https://the-random-gender-ator-api.herokuapp.com/api/v1/sparkles', {
+        body: sparkle
+      }).as('sparkle')
     })
       .visit('http://localhost:3000/')
       .get('.about-btn').click()
