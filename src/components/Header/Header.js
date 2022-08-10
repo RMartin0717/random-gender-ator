fc4a299b66632231847098eca388ccaa2fc292b2
Riley Martin <riley.willow@gmail.com>
2022-08-10 15:07:12 -0500

    Update README and fix missing classes in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,8 +9,8 @@ const Header = () => {
         <h1 className='page-heading'>The Random Gender-ator</h1>
         <div className="button-container">
           <Link to='/'><button className='link-btn'>Home</button></Link>
-          <Link to='/about'><button className='link-btn'>About</button></Link>
-          <Link to='/savedgenders'><button className='link-btn'>Saved Genders</button></Link>
+          <Link to='/about'><button className='link-btn about-btn'>About</button></Link>
+          <Link to='/savedgenders'><button className='link-btn saved-btn'>Saved Genders</button></Link>
         </div>
       </header>
     </div>
